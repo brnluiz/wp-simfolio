ae70714e76a0e4701e68b24ac3ef951100950d16
Bruno Luiz da Silva <contato@brunoluiz.net>
2015-04-04 14:57:28 -0300

    Initial commit

---
+++ portfolio.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Image Portfolio
+ * Plugin URI: #
+ * Version: 1.0
+ * Author: Bruno Luiz
+ * Author URI: http://brunoluiz.net
+ * Description: A simple portfolio system for designers
+ * License: GPL2
+ */
+?>
